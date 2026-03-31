ec483ecf155fbbcd5cbd79d79111711c9fc3705f
Mari-0-0 <gammergamergirrlgirl@gmail.com>
2026-03-31 13:45:41 -0300

    Create ShoppingCart.vue

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="shopping-cart">
+    <h2>Produtos Disponíveis</h2>
+    <ul>
+      <li v-for="product in inventory" :key="product.id">
+        {{ product.name }} - R$ {{ product.price }}
+        <button @click="addProduct(product)">Adicionar ao Carrinho</button>
+      </li>
+    </ul>
+
+    <hr />
+    <h3>Resumo do Carrinho</h3>
+    <p>Itens no carrinho: <strong>{{ cartCount }}</strong></p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    inventory() {
+      return this.$store.state.inventory;
+    },
+    cartCount() {
+      return this.$store.getters.cartItemCount;
+    }
+  },
+  methods: {
+    addProduct(product) {
+      this.$store.dispatch('addToCart', product);
+    }
+  }
+}
+</script>
